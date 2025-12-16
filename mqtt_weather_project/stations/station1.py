a58a2caa2b074ff9c67b76b5258302965d513f62
Cooli <dishan@bluewin.ch>
2025-12-16 18:54:32 +0100

    fix: changed the structure of the code to run the test without error

--- mqtt_weather_project/stations/station1.py
+++ mqtt_weather_project/stations/station1.py
@@ -5,34 +5,6 @@ import json
 import paho.mqtt.client as mqtt
 
 # Das ist ein Testkommentar um den MR zu testen
-broker = "mosquitto"
-port = 1883
-topic = "weather"
-
-station_id = os.getenv("STATION_ID", "WS-XX")
-interval = int(os.getenv("INTERVAL", "5"))
-random
-client = mqtt.Client()
-client.connect(broker, port, 60)
-
-while True:
-    if random.random() < 0.01:
-        temperature = -999
-    else:
-        temperature = round(random.uniform(15, 30), 1)
-    if random.random() < 0.005:
-        print("Simulierter Totalausfall")
-        break
-
-    data = {
-        "stationId": station_id,
-        "temperature": temperature,
-        "humidity": round(random.uniform(30, 60), 1),
-        "timestamp": time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
-    }
-    client.publish(topic, json.dumps(data))
-    print(f"[{station_id}] Published: {data}")
-    time.sleep(interval)
 
 
 def create_data_for_test(station_id):
@@ -42,3 +14,33 @@ def create_data_for_test(station_id):
         "humidity": round(random.uniform(30, 60), 1),
         "timestamp": time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
     }
+
+
+if __name__ == "__main__":
+    broker = "mosquitto"
+    port = 1883
+    topic = "weather"
+
+    station_id = os.getenv("STATION_ID", "WS-XX")
+    interval = int(os.getenv("INTERVAL", "5"))
+    client = mqtt.Client()
+    client.connect(broker, port, 60)
+
+    while True:
+        if random.random() < 0.01:
+            temperature = -999
+        else:
+            temperature = round(random.uniform(15, 30), 1)
+        if random.random() < 0.005:
+            print("Simulierter Totalausfall")
+            break
+
+        data = {
+            "stationId": station_id,
+            "temperature": temperature,
+            "humidity": round(random.uniform(30, 60), 1),
+            "timestamp": time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
+        }
+        client.publish(topic, json.dumps(data))
+        print(f"[{station_id}] Published: {data}")
+        time.sleep(interval)
